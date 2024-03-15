5b8516f0161a458172c70ad0cf90f0fe27f4a9d3
ashraffdanial <ashraffdanial1996@gmail.com>
2024-03-15 12:25:52 +0800

    test

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-
 import dash
+import index
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 server = app.server  # Expose the server variable for deployments
+
+index.registercallbacks(app)
